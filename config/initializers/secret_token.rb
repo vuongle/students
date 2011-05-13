ce8e1440bc77035634103ac9ce70a9ef70a5c5a9
vuongle <leprovic1986vn@yahoo.com>
2011-05-13 22:21:10 +0700

    add config dir to github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Students::Application.config.secret_token = '5faaba2dde308fc986f0b3c5d487ba41eff5edfc501b812ba6f3f1204ffc48265fe4ac3504b9145bf71c7195574233954fc94984bda93b07d03eaaa9d8de4288'
